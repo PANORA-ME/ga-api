da95b551ed05c9c5d0b2290f335bfbdf5f339254
Robin Vervackt <robin.vckt@gmail.com>
2019-03-11 16:53:12 +0100

    fix fs.writeFile requires a callback on node 10

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ module.exports = function(args, callback, settings){
                 concurrentUp();
                 jwt.authorize(function(err, result){
                     concurrentDown();
-                    fs.writeFile(sessionFile, JSON.stringify(result));
+                    fs.writeFile(sessionFile, JSON.stringify(result), (err) => { if (err) { throw err; }});
                     authCallback.apply(this, arguments);
                 });
             });
